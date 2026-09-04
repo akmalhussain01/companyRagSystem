39d398247813e3bf29c8e6d800fddc06e62ff059
akmalhussain01 <akmalhuss313@gmail.com>
2026-09-04 18:57:04 +0500

    first task is done

--- docload.js
+++ docload.js
@@ -1,11 +1,10 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
 
 const docload = async (filepath) => {
-    const loader = new PDFLoader(filepath)
-    const docs = await loader.load()
+    const loader = new PDFLoader(filepath, { splitPages: false })
+    const doc = await loader.load()
 
-    console.log(docs);
-
+    console.log(doc[0].pageContent);
 }
 
 export { docload }
