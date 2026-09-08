360b2b2c6c84cb205b59501c7580bb885f6e692c
akmalhussain01 <akmalhuss313@gmail.com>
2026-09-08 14:46:18 +0500

    adding the chunk of the doc

--- docload.js
+++ docload.js
@@ -1,10 +1,21 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 const docload = async (filepath) => {
+    //load the doc
     const loader = new PDFLoader(filepath, { splitPages: false })
     const doc = await loader.load()
 
     console.log(doc[0].pageContent);
+
+    //chunk the doc
+
+    const text_splitter = new RecursiveCharacterTextSplitter({
+        chunk_size: 500,
+        chunk_overlap: 0
+    });
+    const texts = await text_splitter.splitText(doc[0].pageContent)
+    console.log(texts.length)
 }
 
 export { docload }
