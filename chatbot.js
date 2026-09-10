4eb5b707716fbbe9ed7feb9aa9c3a670c6eb6a95
akmalhussain01 <akmalhuss313@gmail.com>
2026-09-10 23:12:28 +0500

    completing the rag basic and now creating the frontend for the rag system

--- chatbot.js
+++ chatbot.js
@@ -5,34 +5,44 @@ import { vectorStore } from './docload.js';
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function chatbot() {
+
     const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
     while (true) {
 
         const question = await rl.question('Ask a question (or type "exit" to quit): ');
-        console.log(question);
+        // console.log(question);
 
         if (question.toLowerCase() === 'exit') {
             break;
         }
         const relevantchunks = await vectorStore.similaritySearch(question, 3);
         const context = relevantchunks.map((chunk) => chunk.pageContent).join("\n\\n");
-        console.log(context);
+        // console.log(context);
+
+        const SYSTEM_PROMPT = `You are a helpful assistant that answers questions based on the provided context. If the answer is not contained within the context, respond with "I don't know."`;
+
+        const userPrompt = `Context:\n${context}\n\nQuestion: ${question}\nAnswer:`;
+
+        const completion = await groq.chat.completions.create({
+            model: "groq/compound",
+            messages: [
+                {
+                    role: "system",
+                    content: SYSTEM_PROMPT,
+                },
+                {
+                    role: "user",
+                    content: userPrompt,
+                },
+            ],
+        });
+        console.log(`Assistant: ${completion.choices[0].message.content}`);
 
     }
 
     rl.close();
 
-    // const completion = await groq.chat.completions.create({
-    //     model: "groq/compound",
-    //     messages: [
-    //         {
-    //             role: "user",
-    //             content: "Explain why fast inference is critical for reasoning models",
-    //         },
-    //     ],
-    // });
-    // console.log(completion.choices[0]?.message?.content);
 }
 
 chatbot();
